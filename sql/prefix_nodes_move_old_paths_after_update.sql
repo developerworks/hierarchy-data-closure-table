16e2c7fc623a2bdf883d882c6691debf632df6ba
bill2004158 <bill2004158@sina.com>
2014-02-03 02:17:14 +0800

    Update prefix_nodes_move_old_paths_after_update.sql
    fix comment

--- sql/prefix_nodes_move_old_paths_after_update.sql
+++ sql/prefix_nodes_move_old_paths_after_update.sql
@@ -9,9 +9,9 @@ CREATE
     TRIGGER `prefix_nodes_move_old_paths_after_update` AFTER UPDATE ON `prefix_nodes` 
     FOR EACH ROW BEGIN
     IF OLD.`parent_id` != NEW.`parent_id` THEN
-        //http://www.mysqlperformanceblog.com/2011/02/14/moving-subtrees-in-closure-table/
-        //in the example, when change node D's parent to B.
-        //its sql has only D & B. so I think it should currentNode.id& newParent.id
+        --http://www.mysqlperformanceblog.com/2011/02/14/moving-subtrees-in-closure-table/
+        --in the example, when change node D's parent to B.
+        --its sql has only D & B. so I think it should currentNode.id& newParent.id
         CALL p_prefix_nodes_move_old_paths_after_update(NEW.`id`, NEW.`parent_id`);
     END IF;
 
